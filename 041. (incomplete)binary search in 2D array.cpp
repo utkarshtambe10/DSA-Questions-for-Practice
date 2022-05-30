99f55e47cafc2d11b41ce48671be7fcf141365d4
Utkarsh Tambe <89902664+utkarshtambe10@users.noreply.github.com>
2022-05-30 19:05:03 +0530

    Create 041. (incomplete)binary search in 2D array.cpp

---
+++ 041. (incomplete)binary search in 2D array.cpp
@@ -0,0 +1,9 @@
+74. Search a 2D Matrix - LeetCode
+Write an efficient algorithm that searches for a value target in an m x n integer matrix. This matrix has the following properties:
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+  
+Input: matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], target = 3
+Output: true
+  
+Code:
